125dd64f1afeaaf159aa1249b5484abf8bbdcd7d
ShivaKanchi <shivakanchi111@gmail.com>
2023-03-02 21:44:44 +0530

    File added

---
+++ Server/src/index.js
@@ -0,0 +1,16 @@
+import express from 'express';
+
+const mobileshop = express(); //like app  
+
+
+mobileshop.use(express.json());
+
+mobileshop.get("/", (req, res) => {
+    res.json({
+        message: "Server is up"
+    });
+});
+mobileshop.listen(4000, () => {
+    console.log("Server is up"); //port 4000
+
+});
