d908aebf33a3accbfa17a11d2f7ea8747f61cc79
ShivaKanchi <shivakanchi111@gmail.com>
2023-03-04 23:15:26 +0530

    Added mobile mode

--- Server/src/Database/Mobile/index.js
+++ Server/src/Database/Mobile/index.js
@@ -1,1 +1,6 @@
-hello
+import mongoose from "mongoose";
+const MobileSchema = new mongoose.Schema({
+    mobilename: { type: String, required: true },
+    price: { type: Number, require: true }
+})
+export const MobileModel = mongoose.model("mobiles", MobileSchema)
