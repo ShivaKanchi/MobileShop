cf92b395f0be500da41460967ad365dd0ad9b720
ShivaKanchi <shivakanchi111@gmail.com>
2023-03-06 23:24:59 +0530

    Added user model

---
+++ Server/src/Database/User/index.js
@@ -0,0 +1,8 @@
+import mongoose from "mongoose";
+const UserSchema = new mongoose.Schema({
+    username: { type: String, required: true },
+    phone: { type: Number, required: true },
+    email: { type: String, },
+
+})
+export const UserModel = mongoose.model("mobiles", UserSchema)
