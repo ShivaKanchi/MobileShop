93f7136c11d54bb8835d577cf1cfa69f8b53e20d
ShivaKanchi <shivakanchi111@gmail.com>
2023-03-08 22:36:32 +0530

    User model updated

--- Server/src/Database/User/index.js
+++ Server/src/Database/User/index.js
@@ -3,5 +3,7 @@ const UserSchema = new mongoose.Schema({
     username: { type: String, required: true },
     phone: { type: Number, required: true },
     email: { type: String, },
+    address: { type: String, },
+
 })
 export const UserModel = mongoose.model("mobiles", UserSchema)
