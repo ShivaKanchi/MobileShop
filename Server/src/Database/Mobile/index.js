829ad3b2e74f872cd3dff5bcd4cd4e24d1029fca
ShivaKanchi <shivakanchi111@gmail.com>
2024-08-25 15:34:41 +0530

    setup

--- Server/src/Database/Mobile/index.js
+++
@@ -1,6 +0,0 @@
-import mongoose from "mongoose";
-const MobileSchema = new mongoose.Schema({
-    mobilename: { type: String, required: true },
-    price: { type: Number, require: true }
-})
-export const MobileModel = mongoose.model("mobiles", MobileSchema)
--- Server/src/Database/User/index.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-const UserSchema = new mongoose.Schema({
-    username: { type: String, required: true },
-    phone: { type: Number, required: true },
-    email: { type: String, },
-    address: { type: String, },
-
-})
-export const UserModel = mongoose.model("mobiles", UserSchema)
