a2cec6a42476b20d6d15199f2a5d44114213a306
ShivaKanchi <shivakanchi111@gmail.com>
2024-10-17 22:41:16 +0530

    update

--- src/slices/HeroBanner/index.js
+++ src/slices/HeroBanner/index.js
@@ -1,9 +1,12 @@
+import Link from "next/link";
+
 /**
  * @typedef {import("@prismicio/client").Content.HeroBannerSlice} HeroBannerSlice
  * @typedef {import("@prismicio/react").SliceComponentProps<HeroBannerSlice>} HeroBannerProps
  * @param {HeroBannerProps}
  */
 const HeroBanner = ({ slice }) => {
+  console.log(slice.primary);
   return (
     <section
       data-slice-type={slice.slice_type}
@@ -11,6 +14,7 @@ const HeroBanner = ({ slice }) => {
     >
       <h1>{slice.primary.heading}</h1>
       <span>{slice.primary.subheading}</span>
+      {/* <Link href={slice.primary.explorecta}>{slice.primary.explorecta}</Link> */}
     </section>
   );
 };
